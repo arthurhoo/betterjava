3b74bbac9de11d9b9985f6f0dcf2fe4fbf1e276f
huhao <huhao@patsnap.com>
2023-10-31 23:53:56 +0800

    add new module -- algorithm

---
+++ algorithm/sort/src/main/java/com/huhao/code/algorithm/Main.java
@@ -0,0 +1,11 @@
+package com.huhao.code.algorithm;
+
+/**
+ * @author: huhao
+ * @create: 2023/10/31
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
